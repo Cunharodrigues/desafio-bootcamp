6dffbfa2c7a54242ec7eb69ebeebb2472ef648e1
cunharodrigues <pcunha@gmail.com>
2021-10-18 15:35:46 -0300

    h2 database

---
+++ backend/src/main/java/com/devsuperior/dscliente/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscliente.repositories;
+
+import com.devsuperior.dscliente.entities.Client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class ClientRepository extends JpaRepository<Client, Long> {
+
+}
---
+++ backend/src/main/java/com/devsuperior/dscliente/services/ClientService.java
@@ -0,0 +1,64 @@
+package com.devsuperior.dscliente.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import javax.xml.bind.DataBindingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscliente.entities.Client;
+import com.devsuperior.dscliente.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(ententy);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirtDate(dto.getBirtDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+
+	}
+
+	public void delete(Long id) {
+		Try {
+		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("If not found" + id);
+		}catch(DataIntegrityViolationException e) {
+			Throw new DataBindingException("Integrity violation");
+		}
+	}
+}
